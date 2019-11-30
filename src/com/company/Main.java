8623d8e37db2a6381affcf50ff87ce37a22145e0
Krzysztof Zarębski <56186592+Krzysztof148@users.noreply.github.com>
2019-11-30 14:46:02 +0100

    dodano wizytowke

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,10 +1,28 @@
 package com.company;
 
-public class Main {
+import java.util.Scanner;
 
+public class Main {
+    static void wyswietl() {
+        String nazwisko;
+        String imie;
+        int wzrost;
+        String adres;
+
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("nazwisko: ");
+        nazwisko = scanner.next();
+        System.out.println("wzrost: ");
+        wzrost = scanner.next();
+
+
+
+    }
     public static void main(String[] args) {
 
+    wyswietl();
 
-        System.out.println("Napis");
+
     }
 }
